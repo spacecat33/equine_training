f2d2908ac11d0cc4578952993c8086cf1211ed82
Janan Akkad <jananakkad@hotmail.co.uk>
2023-01-25 16:12:41 +0000

    updated protected controller in preparation for building out models and controllers requiring authentication

--- app/controllers/protected_controller.rb
+++ app/controllers/protected_controller.rb
@@ -1,6 +1,10 @@
 class ProtectedController < ApplicationController
-    #  before_action :authenticate_user!
-    
+     before_action :authenticate_user!
+
+    #  i.e.  def redirect_if_not_logged_in
+        # redirect_to new_user_session_path unless user_signed_in?
+    # end
+
     # then make future controllers where you want this 
     # 'before_action' applied to inherit from 
     # protection_controller instead of 
