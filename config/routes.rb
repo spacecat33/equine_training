e5270524003fa81d6ef3e867317975a5318ccc48
Janan Akkad <jananakkad@hotmail.co.uk>
2023-01-25 16:26:31 +0000

    added routes to welcome page and added sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root to: "home#index"
+  root to: "welcome#home" 
+  get '/auth/github/callback' => 'sessions#create'
+  #change 'home' to your chosen controller/model e.g. students
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
